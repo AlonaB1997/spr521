da364ff39bc3e30bf605b37f794dccc52c30f6ce
Alona Bilohorska <alenabelogorskaya@gmail.com>
2025-11-11 15:17:15 +0100

    Added final version of warehouse project and file with functions to it

--- Warehouse.cpp
+++ Warehouse.cpp
@@ -1,257 +1,252 @@
 #include <iostream>
 using namespace std;
 
-
-const int NAME_SIZE = 50;
-const int MANUFACTURER_SIZE = 50;
-const int CATEGORY_SIZE = 30;
-const int DATE_OF_RECEIPT_SIZE = 10;
-const int DATE_OF_EXPIRY_SIZE = 10;
-
-struct Product // Product object structure
-{
-	char name[NAME_SIZE];
-	char manufacturer[MANUFACTURER_SIZE];
-	char category[CATEGORY_SIZE];
-	char dateOfReceipt[DATE_OF_RECEIPT_SIZE];
-	char dateOfExpiry[DATE_OF_EXPIRY_SIZE];
-	float price;
-	size_t id;
-
-};
-
-void fillProductEmpty(Product& product) // Filling product ID with zero value in order to count empty products in the warehouse at the beginning
-{
-	product.id = 0;
-	strcpy_s(product.name, "");
-	strcpy_s(product.manufacturer, "");
-	strcpy_s(product.category, "");
-	strcpy_s(product.dateOfReceipt, "");
-	strcpy_s(product.dateOfExpiry, "");
-	product.price = 0.0f;
-
-}
-
-void fillWarehouseEmpty(Product* warehouse, size_t capacity) // Filling the whole warehouse with empty products
-{
-	for (size_t i = 0; i < capacity; i++)
-	{
-		fillProductEmpty(warehouse[i]);
-	}
-}
-
-size_t countExistingProducts(Product* warehouse, size_t capacity) // Counting existing products in the warehouse by checking product IDs
-{
-	size_t count = 0;
-	for (size_t i = 0; i < capacity; i++)
-	{
-		if (warehouse[i].id != 0)
-		{
-			count++;
-		}
-	}
-
-	return count;
-}
+#include "Warehouse_f.h"
 
 
-void fillProduct(Product& product) // Filling product with user input data. Id is populated automatically through function addProduct()
-{
-	cout << "Enter product name: ";
-	cin >> product.name;
-	cout << "Enter manufacturer: ";
-	cin >> product.manufacturer;
-	cout << "Enter category: ";
-	cin >> product.category;
-	cout << "Enter date of receipt (DD.MM.YYYY): ";
-	cin >> product.dateOfReceipt;
-	cout << "Enter date of expiry (DD.MM.YYYY): ";
-	cin >> product.dateOfExpiry;
-	cout << "Enter price: ";
-	cin >> product.price;
-
-}
-
-void fillProductId(Product* warehouse, size_t count)
-{
-	for (size_t i = 0; i < count; i++)
-	{
-		warehouse[i].id = i + 1;
-	}
-}
-
-void showProduct(const Product& product) // Displaying product information
-{
-	cout << "Product ID: " << product.id << endl;
-	cout << "Product name: " << product.name << endl;
-	cout << "Manufacturer: " << product.manufacturer << endl;
-	cout << "Category: " << product.category << endl;
-	cout << "Date of receipt: " << product.dateOfReceipt << endl;
-	cout << "Date of expiry: " << product.dateOfExpiry << endl;
-	cout << "Price: " << product.price << endl;
-}
-
-void showWarehouse(Product* warehouse, size_t count) // Displaying all products in the warehouse
-{
-	for (size_t i = 0; i < count; i++)
-	{
-		cout << "Product #" << i + 1 << ": " << endl;
-		showProduct(warehouse[i]);
-	}
-}
-
-Product* addProduct(Product* warehouse, size_t& capacity, size_t& count, size_t productsToAdd) // Adding products to the warehouse. If there is not enough capacity, 
-// then creating a new warehouse with adding products from old warehouse and adding new products
-{
-	if (warehouse != nullptr)
-	{
-		if (count + productsToAdd <= capacity)
-		{
-			for (size_t i = 0; i < productsToAdd; i++)
-			{
-				fillProduct(warehouse[count + i]);
-			}
-
-			count += productsToAdd;
-
-			fillProductId(warehouse, count);
-		}
-		else
-		{
-			size_t newCapacity = count + productsToAdd;
-			Product* newWarehouse = new Product[newCapacity];
-
-			for (size_t i = 0; i < newCapacity; i++)
-			{
-				if (i < count)
-				{
-					newWarehouse[i] = warehouse[i];
-				}
-				else
-				{
-					fillProduct(newWarehouse[i]);
-				}
-
-			}
-
-			delete[] warehouse;
-			warehouse = newWarehouse;
-			capacity = newCapacity;
-			count += productsToAdd;
-
-			fillProductId(warehouse, count);
-		}
-	}
-
-	else
-	{
-		cout << "Warehouse doesn't exist!";
-	}
-
-	return warehouse;
-}
-
-
-Product* removeProduct(Product* warehouse, size_t& capacity, size_t& count, int idToRemove)
-{
-	size_t newCapacity = --count;
-	Product* newWarehouse = new Product[newCapacity];
-
-	for (size_t i = 0, j = 0; j < newCapacity; i++)
-	{
-		if (warehouse[i].id != idToRemove)
-		{
-			newWarehouse[j++] = warehouse[i];
-
-		}
-
-	}
-
-	delete[] warehouse;
-	warehouse = newWarehouse;
-	capacity = newCapacity;
-
-	return warehouse;
-}
-
-bool checkProductId(Product* warehouse, size_t capacity, int idToRemove)
-{
-	bool found = false;
-
-	for (size_t i = 0; i < capacity; i++)
-	{
-		if (warehouse[i].id == idToRemove)
-		{
-			found = true;
-		}
-	}
-
-	return found;
-}
 
 
 int main()
 {
-	int choice, idToRemove;
-	bool foundIdToRemove;
-	size_t capacity, count, productsToAdd;
+	int choice; // To choose type of operation
+	bool foundId; // To find if specific ID for operation 2, 3 or 4 has been found
+	size_t capacity, count;
 	cout << "Enter warehouse capacity: ";
 	cin >> capacity;
 
 	Product* warehouse = new Product[capacity]; // Creating warehouse with given capacity
 
 	fillWarehouseEmpty(warehouse, capacity); // Filling warehouse with empty products
+	count = countExistingProducts(warehouse, capacity);
 
-	count = countExistingProducts(warehouse, capacity); // Counting existing products in the warehouse for future checks
+	cout << "\nCurrently in the warehouse: " << count << " products." << endl; // Counting existing products in the warehouse for future checks
 
 
-	do
-	{
-		cout << "Choose operation (1 - Add product, 2 - Remove product, 3 - Change product, 4 - Search product, 5 - Sort products, 0 - Exit): ";
+	do {
+		cout << "\nChoose operation (1 - Add product, 2 - Remove product, 3 - Change product, 4 - Search product, 5 - Sort products, 0 - Exit programm): ";
 		cin >> choice;
-		switch (choice)
-		{
-		case 1:
-			cout << "Enter number of products to add: ";
-			cin >> productsToAdd;
 
-			warehouse = addProduct(warehouse, capacity, count, productsToAdd);
+		if (choice >= 1 && choice <= 5) {
+			switch (choice) {
+			case 1:
+				size_t productsToAdd;
 
-			cout << "Products have been successfully added to the warehouse!" << endl;
-			cout << "Products in the warehouse after addition: " << endl;
-			showWarehouse(warehouse, count);
-			break;
+				cout << "\nEnter number of products to add: ";
+				cin >> productsToAdd;
 
-		case 2:
-			do {
-				cout << "Enter ID of product to remove: ";
-				cin >> idToRemove;
-				foundIdToRemove = checkProductId(warehouse, capacity, idToRemove);
+				warehouse = addProduct(warehouse, capacity, count, productsToAdd);
 
-				if (foundIdToRemove)
-				{
-					warehouse = removeProduct(warehouse, capacity, count, idToRemove);
-					cout << "Products have been successfully removed from the warehouse!" << endl;
-					cout << "Products in the warehouse after removal: " << endl;
-					showWarehouse(warehouse, count);
-				}
-				else
-				{
-					cout << "Product ID to remove has not been found. Please try again." << endl;
-				}
+				cout << "\nProducts have been successfully added to the warehouse!" << endl;
+				cout << "\nProducts in the warehouse after addition: " << endl;
+				showWarehouse(warehouse, count);
+				break;
 
-			} while (!foundIdToRemove);
+			case 2:
+				size_t idToRemove;
+
+				do {
+					cout << "\nEnter ID of product to remove or 0 (to exit operation): ";
+					cin >> idToRemove;
+					foundId = checkProductId(warehouse, capacity, idToRemove);
+
+					if (foundId && idToRemove != 0) {
+
+						warehouse = removeProduct(warehouse, capacity, count, idToRemove);
+						cout << "\nProduct has been successfully removed from the warehouse!" << endl;
+						cout << "\nProducts in the warehouse after removal: " << endl;
+						fillProductId(warehouse, count);
+						showWarehouse(warehouse, count);
+
+					}
+					else if (idToRemove == 0) {
+						cout << "\nExiting operation..." << endl;
+					}
+					else {
+
+						cout << "\nProduct ID to remove has not been found. Please try again." << endl;
+
+					}
+
+				} while (idToRemove != 0);
+				break;
+
+			case 3:
+				size_t idToChange;
+
+				do {
+					cout << "\nEnter ID product to change or 0 (to exit operation): ";
+					cin >> idToChange;
+					foundId = checkProductId(warehouse, capacity, idToChange);
+
+					if (foundId && idToChange != 0) {
+
+						changeProduct(warehouse, capacity, idToChange);
+						cout << "\nProduct has been successfully changed!" << endl;
+						cout << "\nProducts in the warehouse after change: " << endl;
+						fillProductId(warehouse, count);
+						showWarehouse(warehouse, count);
+
+					}
+					else if (idToChange == 0) {
+						cout << "\nExiting operation..." << endl;
+					}
+					else {
+
+						cout << "\nProduct ID to change has not been found. Please try again." << endl;
+					}
+
+				} while (idToChange != 0);
+				break;
+
+			case 4:
+				int searchChoice, searchId;
+
+				do {
+					cout << "\nEnter criteria for search (1 - by name, 2 - by manufacturer, 3 - by price, 4 - by category, 5 - by date of receipt, 6 - by date of expiry, 0 - to exit operation): ";
+					cin >> searchChoice;
+					cin.ignore();
+
+					if (searchChoice >= 1 && searchChoice <= 6) {
+						switch (searchChoice) {
+						case 1:
+							char searchName[NAME_SIZE];
+							cout << "\nEnter search name: ";
+							cin.getline(searchName, NAME_SIZE);
+							searchId = searchChar(warehouse, capacity, searchName, searchChoice);
+							if (searchId != -1) {
+								showProduct(warehouse[searchId - 1]);
+							}
+							else {
+								cout << "\nProduct has not been found!" << endl;
+							}
+							break;
+
+						case 2:
+							char searchManufacturer[MANUFACTURER_SIZE];
+							cout << "\nEnter search manufacturer's name: ";
+							cin.getline(searchManufacturer, MANUFACTURER_SIZE);
+							searchId = searchChar(warehouse, capacity, searchManufacturer, searchChoice);
+							if (searchId != -1) {
+								showProduct(warehouse[searchId - 1]);
+							}
+							else {
+								cout << "\nProduct has not been found!" << endl;
+							}
+							break;
+
+						case 3:
+							float searchPrice;
+							cout << "\nEnter search price: ";
+							cin >> searchPrice;
+							searchId = searchFloat(warehouse, capacity, searchPrice, searchChoice);
+							if (searchId != -1) {
+								showProduct(warehouse[searchId - 1]);
+							}
+							else {
+								cout << "\nProduct has not been found!" << endl;
+							}
+							break;
 
 
+						case 4:
+							char searchCategory[CATEGORY_SIZE];
+							cout << "\nEnter search category: ";
+							cin.getline(searchCategory, CATEGORY_SIZE);
+							searchId = searchChar(warehouse, capacity, searchCategory, searchChoice);
+							if (searchId != -1) {
+								showProduct(warehouse[searchId - 1]);
+							}
+							else {
+								cout << "\nProduct has not been found!" << endl;
+							}
+							break;
+
+						case 5:
+							char searchDateOfReceipt[DATE_OF_RECEIPT_SIZE];
+							cout << "\nEnter search date of receipt: ";
+							cin.getline(searchDateOfReceipt, DATE_OF_RECEIPT_SIZE);
+							searchId = searchChar(warehouse, capacity, searchDateOfReceipt, searchChoice);
+							if (searchId != -1) {
+								showProduct(warehouse[searchId - 1]);
+							}
+							else {
+								cout << "\nProduct has not been found!" << endl;
+							}
+							break;
+
+						case 6:
+							char searchDateOfExpiry[DATE_OF_EXPIRY_SIZE];
+							cout << "\nEnter search date of expiry: ";
+							cin.getline(searchDateOfExpiry, DATE_OF_EXPIRY_SIZE);
+							searchId = searchChar(warehouse, capacity, searchDateOfExpiry, searchChoice);
+							if (searchId != -1) {
+								showProduct(warehouse[searchId - 1]);
+							}
+							else {
+								cout << "\nProduct has not been found!" << endl;
+							}
+							break;
+						}
+					}
+					else if (searchChoice == 0) {
+						cout << "\nExiting operation..." << endl;
+					}
+					else {
+						cout << "\nEnter valid criteria for search!" << endl;
+					}
+
+				} while (searchChoice != 0);
+				break;
+
+			case 5:
+				int sortChoice;
+
+				do {
+					cout << "\nEnter criteria for sort (1 - by price, 2 - by category, 0 - to exit operation): ";
+					cin >> sortChoice;
+
+					if (sortChoice >= 1 && sortChoice <= 2) {
+						switch (sortChoice) {
+						case 1:
+							sortByPrice(warehouse, capacity);
+							fillProductId(warehouse, count);
+							cout << "\nWarehouse after sort by price: " << endl;
+							showWarehouse(warehouse, count);
+							break;
+
+						case 2:
+							sortByCategory(warehouse, capacity);
+							fillProductId(warehouse, count);
+							cout << "\nWarehouse after sort by category: " << endl;
+							showWarehouse(warehouse, count);
+							break;
+						}
+					}
+					else if (sortChoice == 0) {
+						cout << "\nExiting operation..." << endl;
+					}
+					else {
+						cout << "\nEnter valid criteria for sort!" << endl;
+					}
+
+				} while (sortChoice != 0);
+
+			}
+
+		}
+		else if (choice == 0) {
+			cout << "\nExiting programm.." << endl;
+		}
+		else {
+			cout << "\nEnter valid operation for programm!" << endl;
 		}
 
 	} while (choice != 0);
 
 
 
-
-
-
+	delete[] warehouse;
 
 	return 0;
+
 }
